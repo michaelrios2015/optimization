26a0a89573d7d134e0e099357b940cd5750a339f
Michael Rios <michael.rios093@gmail.com>
2015-12-19 20:12:16 -0500

    optimize web pages

---
+++ js/anaylitics-full.js
@@ -0,0 +1,13 @@
+(function(w, g) {
+                w['GoogleAnalyticsObject'] = g;
+                w[g] = w[g] || function() {
+                    (w[g].q = w[g].q || []).push(arguments)
+                };
+                w[g].l = 1 * new Date();
+            })(window, 'ga');
+
+             // TODO: replace with your Google Analytics profile ID. can we do that??
+            ga('create', 'UA-XXXX-Y');
+            ga('send', 'pageview');
+
+
---
+++ js/anaylitics.js
@@ -0,0 +1,1 @@
+(function(w, g){w['GoogleAnalyticsObject']=g; w[g]=w[g] || function(){(w[g].q=w[g].q || []).push(arguments)}; w[g].l=1 * new Date();})(window, 'ga'); // TODO: replace with your Google Analytics profile ID. can we do that?? ga('create', 'UA-XXXX-Y'); ga('send', 'pageview');
